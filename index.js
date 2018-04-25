df5bb8a9d170a11d82e26d485a55cba214e04009
Tyler Dreher <drehertyler@gmail.com>
2018-04-25 18:43:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  } elseif (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  } elseif (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
 }
