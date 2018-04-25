e06e263e3717bde0afd1ac93715fb4a5c4f67997
Tyler Dreher <drehertyler@gmail.com>
2018-04-25 18:04:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function shout(string) {
   return string.toUpperCase();
 }
+function whisper(string) {
+  return whisper.toLowerCase();
+}
+function logShould(string) {
+  console.log(string.toUpperCase())
+}
 function 
